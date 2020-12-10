fc4bfcfe5e58e8282d9df4309210086e33172d17
ashsvis <ashsvis@gmail.com>
2020-12-10 16:33:15 +0400

    Заготовка интерфейса под Ethernet связь с контроллером

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -49,12 +49,23 @@
             this.cbParity = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.cbSlaveID = new System.Windows.Forms.ComboBox();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.btnFindRefresh = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbIpAddress = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbIpPort = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.tabPage3.SuspendLayout();
             this.tabPage4.SuspendLayout();
             this.tabPage5.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.tabPage6.SuspendLayout();
+            this.tabPage7.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -67,10 +78,10 @@
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Controls.Add(this.tabPage5);
-            this.tabControl1.Location = new System.Drawing.Point(14, 55);
+            this.tabControl1.Location = new System.Drawing.Point(14, 70);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(480, 503);
+            this.tabControl1.Size = new System.Drawing.Size(480, 504);
             this.tabControl1.TabIndex = 2;
             this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
             // 
@@ -102,7 +113,7 @@
             this.tabPage3.Location = new System.Drawing.Point(4, 24);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(472, 475);
+            this.tabPage3.Size = new System.Drawing.Size(472, 476);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "ADC";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -168,7 +179,7 @@
             this.riserTuningAdc.Name = "riserTuningAdc";
             this.riserTuningAdc.NodeAddr = 0;
             this.riserTuningAdc.NodeType = 0;
-            this.riserTuningAdc.Size = new System.Drawing.Size(466, 469);
+            this.riserTuningAdc.Size = new System.Drawing.Size(466, 470);
             this.riserTuningAdc.TabIndex = 0;
             // 
             // riserTuningAlarmLevel
@@ -218,7 +229,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Location = new System.Drawing.Point(6, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 15);
             this.label1.TabIndex = 1;
@@ -228,7 +239,7 @@
             // 
             this.cbPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbPort.FormattingEnabled = true;
-            this.cbPort.Location = new System.Drawing.Point(59, 12);
+            this.cbPort.Location = new System.Drawing.Point(50, 6);
             this.cbPort.Name = "cbPort";
             this.cbPort.Size = new System.Drawing.Size(65, 23);
             this.cbPort.TabIndex = 0;
@@ -237,7 +248,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(130, 15);
+            this.label2.Location = new System.Drawing.Point(121, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(62, 15);
             this.label2.TabIndex = 1;
@@ -247,7 +258,7 @@
             // 
             this.cbBaudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbBaudRate.FormattingEnabled = true;
-            this.cbBaudRate.Location = new System.Drawing.Point(198, 12);
+            this.cbBaudRate.Location = new System.Drawing.Point(189, 6);
             this.cbBaudRate.Name = "cbBaudRate";
             this.cbBaudRate.Size = new System.Drawing.Size(68, 23);
             this.cbBaudRate.TabIndex = 1;
@@ -256,7 +267,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(272, 15);
+            this.label3.Location = new System.Drawing.Point(263, 9);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(55, 15);
             this.label3.TabIndex = 1;
@@ -266,44 +277,128 @@
             // 
             this.cbParity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbParity.FormattingEnabled = true;
-            this.cbParity.Location = new System.Drawing.Point(333, 12);
+            this.cbParity.Location = new System.Drawing.Point(324, 6);
             this.cbParity.Name = "cbParity";
             this.cbParity.Size = new System.Drawing.Size(58, 23);
-            this.cbParity.TabIndex = 1;
+            this.cbParity.TabIndex = 2;
             this.cbParity.SelectionChangeCommitted += new System.EventHandler(this.cbParity_SelectionChangeCommitted);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(397, 15);
+            this.label4.Location = new System.Drawing.Point(17, 3);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(43, 15);
+            this.label4.Size = new System.Drawing.Size(70, 15);
             this.label4.TabIndex = 1;
-            this.label4.Text = "Адрес:";
+            this.label4.Text = "Адрес узла:";
             // 
             // cbSlaveID
             // 
             this.cbSlaveID.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbSlaveID.FormattingEnabled = true;
-            this.cbSlaveID.Location = new System.Drawing.Point(446, 12);
+            this.cbSlaveID.Location = new System.Drawing.Point(18, 21);
             this.cbSlaveID.Name = "cbSlaveID";
-            this.cbSlaveID.Size = new System.Drawing.Size(48, 23);
-            this.cbSlaveID.TabIndex = 1;
+            this.cbSlaveID.Size = new System.Drawing.Size(67, 23);
+            this.cbSlaveID.TabIndex = 0;
             this.cbSlaveID.SelectionChangeCommitted += new System.EventHandler(this.cbSlaveID_SelectionChangeCommitted);
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage6);
+            this.tabControl2.Controls.Add(this.tabPage7);
+            this.tabControl2.Location = new System.Drawing.Point(94, 6);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(400, 61);
+            this.tabControl2.TabIndex = 1;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.cbPort);
+            this.tabPage6.Controls.Add(this.label1);
+            this.tabPage6.Controls.Add(this.cbParity);
+            this.tabPage6.Controls.Add(this.label2);
+            this.tabPage6.Controls.Add(this.cbBaudRate);
+            this.tabPage6.Controls.Add(this.label3);
+            this.tabPage6.Location = new System.Drawing.Point(4, 24);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(392, 33);
+            this.tabPage6.TabIndex = 0;
+            this.tabPage6.Text = "Serial Link";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Controls.Add(this.tbIpPort);
+            this.tabPage7.Controls.Add(this.tbIpAddress);
+            this.tabPage7.Controls.Add(this.label6);
+            this.tabPage7.Controls.Add(this.label5);
+            this.tabPage7.Location = new System.Drawing.Point(4, 24);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage7.Size = new System.Drawing.Size(392, 33);
+            this.tabPage7.TabIndex = 1;
+            this.tabPage7.Text = "Ethernet Link";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // btnFindRefresh
+            // 
+            this.btnFindRefresh.Font = new System.Drawing.Font("Segoe UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnFindRefresh.Location = new System.Drawing.Point(18, 47);
+            this.btnFindRefresh.Name = "btnFindRefresh";
+            this.btnFindRefresh.Size = new System.Drawing.Size(67, 20);
+            this.btnFindRefresh.TabIndex = 3;
+            this.btnFindRefresh.Text = "Найти...";
+            this.btnFindRefresh.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Адрес IP:";
+            // 
+            // tbIpAddress
+            // 
+            this.tbIpAddress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbIpAddress.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbIpAddress.Location = new System.Drawing.Point(63, 6);
+            this.tbIpAddress.Name = "tbIpAddress";
+            this.tbIpAddress.Size = new System.Drawing.Size(111, 21);
+            this.tbIpAddress.TabIndex = 0;
+            this.tbIpAddress.Text = "192.168.0.1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(180, 8);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 15);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Номер порта:";
+            // 
+            // tbIpPort
+            // 
+            this.tbIpPort.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbIpPort.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbIpPort.Location = new System.Drawing.Point(269, 6);
+            this.tbIpPort.Name = "tbIpPort";
+            this.tbIpPort.Size = new System.Drawing.Size(47, 21);
+            this.tbIpPort.TabIndex = 1;
+            this.tbIpPort.Text = "502";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(506, 570);
+            this.ClientSize = new System.Drawing.Size(506, 578);
+            this.Controls.Add(this.btnFindRefresh);
+            this.Controls.Add(this.tabControl2);
             this.Controls.Add(this.cbSlaveID);
-            this.Controls.Add(this.cbParity);
-            this.Controls.Add(this.cbBaudRate);
-            this.Controls.Add(this.cbPort);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.tabControl1);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -319,6 +414,11 @@
             this.tabPage3.ResumeLayout(false);
             this.tabPage4.ResumeLayout(false);
             this.tabPage5.ResumeLayout(false);
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage6.ResumeLayout(false);
+            this.tabPage6.PerformLayout();
+            this.tabPage7.ResumeLayout(false);
+            this.tabPage7.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -345,6 +445,14 @@
         private System.Windows.Forms.ComboBox cbParity;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox cbSlaveID;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.Button btnFindRefresh;
+        private System.Windows.Forms.TextBox tbIpAddress;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbIpPort;
+        private System.Windows.Forms.Label label6;
     }
 }
 
